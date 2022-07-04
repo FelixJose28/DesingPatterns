3c1f656907a87e93bac09735c8cb3a1b45e75999
FelixYnfanteCodemera <felix.ynfante@codemera.com>
2022-07-04 13:49:17 -0400

    feat: creational pattern, singleton

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonPattern;
+using System;
+
+var singleton = Singleton.GetInstance;
+Console.WriteLine(singleton.message);
+singleton.message = "modified";
+Console.WriteLine(singleton.message);
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        public string message;
+
+        private Singleton()
+        {
+            message = "initialized";
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    return new Singleton();
+                return instance;
+            }
+        }
+
+
+    }
+}
